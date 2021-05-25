5bb92b7abfa33e8892b013db8aaf5e54f04493bd
Nikhil Saraf <nsaraf98@gmail.com>
2021-05-26 04:43:30 +0530

    react jsx plugin

--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
+import reactJsx from "vite-react-jsx";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [reactRefresh(), tsconfigPaths()],
+  plugins: [reactRefresh(), tsconfigPaths(), reactJsx()],
   base: "./",
   publicDir: "assets",
   define: {
