68e01ffe6268926bf768a167d120ffcd88dc72bb
Nikhil Saraf <nsaraf98@gmail.com>
2023-07-04 00:28:10 +0530

    react spa

---
+++ examples/react-spa/app/client.tsx
@@ -0,0 +1,8 @@
+/// <reference types="vinxi/client" />
+import ReactDOM from "react-dom/client";
+
+import "./style.css";
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+	<div>Hello World</div>,
+);
