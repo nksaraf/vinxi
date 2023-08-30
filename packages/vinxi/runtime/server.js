db90cff6813c26fe0d86df6dac08746e149787f9
Nikhil Saraf <nsaraf98@gmail.com>
2023-08-30 22:54:00 +0530

    expose createMiddleware API from vinxi/runtime/server

--- packages/vinxi/runtime/server.js
+++ packages/vinxi/runtime/server.js
@@ -1,1 +1,16 @@
+import { createAppEventHandler } from "h3";
+
 export * from "h3";
+
+export function createMiddleware(getHandler) {
+	return (app) => {
+		const prevHandler = createAppEventHandler([...app.h3App.stack], {});
+		const handler = getHandler({ forward: (event) => prevHandler(event) });
+
+		app.h3App.stack.unshift({
+			route: "/",
+			match: undefined,
+			handler: handler,
+		});
+	};
+}
