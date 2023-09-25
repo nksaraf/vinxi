6589a694daec78008b6149e776b3f64e89f1cab7
Nikhil Saraf <nsaraf98@gmail.com>
2023-09-26 04:05:01 +0530

    cleanup events

--- test/templates/react-ssr-fs/app/server.tsx
+++ test/templates/react-ssr-fs/app/server.tsx
@@ -20,40 +20,45 @@ export default eventHandler(async (event) => {
 	});
 
 	const assets = await clientManifest.inputs[clientManifest.handler].assets();
-	const events = {};
 
 	const base =
 		import.meta.env.BASE_URL === "/" ? undefined : import.meta.env.BASE_URL;
 
-	const stream = renderToPipeableStream(
-		<App assets={<Suspense>{assets.map((m) => renderAsset(m))}</Suspense>}>
-			<Router
-				ssrPath={
-					event.path
-					// event.path
-				}
-				base={base}
-			>
-				<Suspense>
-					{routes.map((route) => (
-						<Route
-							path={route.path}
-							key={route.path}
-							component={route.component}
-						/>
-					))}
-				</Suspense>
-			</Router>
-		</App>,
-		{
-			bootstrapModules: [
-				clientManifest.inputs[clientManifest.handler].output.path,
-			],
-			bootstrapScriptContent: `window.manifest = ${JSON.stringify(
-				await clientManifest.json(),
-			)}; window.base = ${JSON.stringify(base)};`,
-		},
-	);
+	const json = JSON.stringify(await clientManifest.json());
+	const stream = await new Promise((resolve) => {
+		const stream = renderToPipeableStream(
+			<App assets={<Suspense>{assets.map((m) => renderAsset(m))}</Suspense>}>
+				<Router
+					ssrPath={
+						event.path
+						// event.path
+					}
+					base={base}
+				>
+					<Suspense>
+						{routes.map((route) => (
+							<Route
+								path={route.path}
+								key={route.path}
+								component={route.component}
+							/>
+						))}
+					</Suspense>
+				</Router>
+			</App>,
+			{
+				onShellReady() {
+					resolve(stream);
+				},
+				bootstrapModules: [
+					clientManifest.inputs[clientManifest.handler].output.path,
+				],
+				bootstrapScriptContent: `window.manifest = ${json}; window.base = ${JSON.stringify(
+					base,
+				)};`,
+			},
+		);
+	});
 
 	setHeader(event, "Content-Type", "text/html");
 
