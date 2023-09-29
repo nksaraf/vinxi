02bc954daa042866f71a2bb27ec9a28ba1bfa8f7
Nikhil Saraf <nsaraf98@gmail.com>
2023-09-29 15:11:19 +0530

    fix pnpm

--- docs/nuxt.config.ts
+++ docs/nuxt.config.ts
@@ -1,3 +1,31 @@
+import { nodePolyfills } from "vite-plugin-node-polyfills";
+
 export default defineNuxtConfig({
-  extends: '@nuxt-themes/docus'
-})
+	extends: "@nuxt-themes/docus",
+	nitro: {
+		routeRules: {
+			"/**": {
+				headers: {
+					"Cross-Origin-Opener-Policy": "same-origin",
+					"Cross-Origin-Embedder-Policy": "credentialless",
+				},
+			},
+		},
+	},
+	vite: {
+		define: {
+			"process.env.NODE_DEBUG": "false",
+		},
+		server: {
+			headers: {
+				"Cross-Origin-Opener-Policy": "same-origin",
+				"Cross-Origin-Embedder-Policy": "require-corp",
+			},
+		},
+		plugins: [
+			nodePolyfills({
+				globals: true,
+			}),
+		],
+	},
+});
