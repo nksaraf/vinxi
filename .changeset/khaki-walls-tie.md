ad7c1a97f1ac61be625d215d5d0274f9484e1999
Nikhil Saraf <nsaraf98@gmail.com>
2023-11-30 07:21:48 +0530

    docs(changeset): refactor:simplify server-functions server side plugin

---
+++ .changeset/khaki-walls-tie.md
@@ -0,0 +1,5 @@
+---
+"@vinxi/server-functions": patch
+---
+
+refactor:simplify server-functions server side plugin
--- packages/vinxi-server-functions/server.js
+++ packages/vinxi-server-functions/server.js
@@ -1,8 +1,4 @@
-import {
-	directives,
-	splitPlugin,
-	wrapExportsPlugin,
-} from "@vinxi/plugin-directives";
+import { directives, wrapExportsPlugin } from "@vinxi/plugin-directives";
 import { readFileSync } from "fs";
 import { fileURLToPath } from "url";
 import { chunkify } from "vinxi/lib/chunks";
@@ -10,135 +6,86 @@ import { handlerModule, join, normalize } from "vinxi/lib/path";
 
 import { CLIENT_REFERENCES_MANIFEST } from "./constants.js";
 
+export function serverTransform({ runtime }) {
+	return directives({
+		hash: chunkify,
+		runtime: runtime,
+		transforms: [
+			wrapExportsPlugin({
+				runtime: {
+					module: runtime,
+					function: "createServerReference",
+				},
+				hash: chunkify,
+				apply: (code, id, options) => {
+					return options.ssr;
+				},
+				pragma: "use server",
+			}),
+		],
+		onReference(type, reference) {},
+	});
+}
+
+/**
+ *
+ * @returns {import('vinxi').Plugin}
+ */
+export const serverBuild = ({ client, manifest }) => {
+	let input;
+	return {
+		name: "server-functions:build",
+		enforce: "post",
+		apply: "build",
+		config(config, env) {
+			// @ts-ignore
+			const router = config.router;
+			// @ts-ignore
+			const app = config.app;
+
+			const rscRouter = app.getRouter(client);
+
+			const serverFunctionsManifest = JSON.parse(
+				readFileSync(join(rscRouter.outDir, rscRouter.base, manifest), "utf-8"),
+			);
+
+			input = {
+				entry: handlerModule(router),
+				...Object.fromEntries(
+					serverFunctionsManifest.server.map((key) => {
+						return [chunkify(key), key];
+					}),
+				),
+			};
+
+			return {
+				build: {
+					rollupOptions: {
+						output: {
+							chunkFileNames: "[name].js",
+						},
+						treeshake: true,
+					},
+				},
+			};
+		},
+
+		configResolved(config) {
+			config.build.rollupOptions.input = input;
+		},
+	};
+};
+
 /**
  *
  * @returns {import('vinxi').Plugin[]}
  */
 export function server({
 	client = "client",
-	resolve = {
-		conditions: [],
-	},
-	transpileDeps = ["react", "react-dom", "@vinxi/react-server-dom"],
 	manifest = CLIENT_REFERENCES_MANIFEST,
 	runtime = normalize(
 		fileURLToPath(new URL("./server-runtime.js", import.meta.url)),
 	),
-	// onReference
 } = {}) {
-	let isBuild;
-	let input;
-	return [
-		directives({
-			hash: chunkify,
-			runtime: runtime,
-			transforms: [
-				wrapExportsPlugin({
-					runtime: {
-						module: runtime,
-						function: "createServerReference",
-					},
-					// onModuleFound: (mod) => onReference("server", mod),
-					hash: chunkify,
-					apply: (code, id, options) => {
-						return options.ssr;
-					},
-					pragma: "use server",
-				}),
-			],
-			onReference(type, reference) {
-				// if (type === "server") {
-				// 	serverModules.add(reference);
-				// } else {
-				// 	clientModules.add(reference);
-				// }
-			},
-		}),
-		{
-			name: "server-functions:build",
-			enforce: "post",
-			config(config, env) {
-				isBuild = env.command === "build";
-				// @ts-ignore
-				const router = config.router;
-				// @ts-ignore
-				const app = config.app;
-
-				if (isBuild) {
-					const rscRouter = app.getRouter(client);
-
-					const serverFunctionsManifest = JSON.parse(
-						readFileSync(
-							join(rscRouter.outDir, rscRouter.base, manifest),
-							"utf-8",
-						),
-					);
-
-					input = {
-						entry: handlerModule(router),
-						...Object.fromEntries(
-							serverFunctionsManifest.server.map((key) => {
-								return [chunkify(key), key];
-							}),
-						),
-					};
-
-					return {
-						build: {
-							rollupOptions: {
-								output: {
-									chunkFileNames: "[name].js",
-								},
-								treeshake: true,
-							},
-						},
-						ssr: {
-							resolve: {
-								externalConditions: [
-									...(resolve.conditions ?? []),
-									"node",
-									"import",
-									process.env.NODE_ENV,
-								],
-								conditions: [
-									...(resolve.conditions ?? []),
-									"node",
-									"import",
-									process.env.NODE_ENV,
-								],
-							},
-							noExternal: true,
-						},
-					};
-				} else {
-					return {
-						ssr: {
-							resolve: {
-								externalConditions: [
-									...(resolve.conditions ?? []),
-									"node",
-									"import",
-									process.env.NODE_ENV,
-								],
-								conditions: [
-									...(resolve.conditions ?? []),
-									"node",
-									"import",
-									process.env.NODE_ENV,
-								],
-							},
-							external: transpileDeps,
-							noExternal: true,
-						},
-					};
-				}
-			},
-
-			configResolved(config) {
-				if (isBuild) {
-					config.build.rollupOptions.input = input;
-				}
-			},
-		},
-	];
+	return [serverTransform({ runtime }), serverBuild({ client, manifest })];
 }
