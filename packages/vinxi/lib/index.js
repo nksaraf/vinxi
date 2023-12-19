4ef9c83ae4cfcc778fb87004ca28f4df03095939
Brenley Dueck <brenleydueck@gmail.com>
2023-12-18 22:25:10 -0600

    fix: add types

--- packages/vinxi/lib/index.js
+++ packages/vinxi/lib/index.js
@@ -3,3 +3,5 @@ export * from "./app.js";
 /** @typedef {import('vite').Plugin} Plugin */
 export { resolve } from "./resolve.js";
 export * from "./router-modes.js";
+
+/** @typedef {import('nitropack').NitroConfig} NitroConfig */
