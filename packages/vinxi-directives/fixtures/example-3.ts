e4356bad27920e7740a2b4de7bbb87f719c0032c
Nikhil Saraf <nsaraf98@gmail.com>
2024-01-11 04:34:02 +0530

    fixes Exporting type definitions from a "use server" file doesn't work. #106

---
+++ packages/vinxi-directives/fixtures/example-3.ts
@@ -0,0 +1,45 @@
+"use runtime";
+
+import { type InferSelectModel, sql } from "drizzle-orm";
+import { mysqlTable, timestamp, varchar } from "drizzle-orm/mysql-core";
+import { db } from "~/db";
+import { nanoid } from "~/utils/nanoid";
+
+const organisations = mysqlTable("organisations", {
+	id: varchar("id", { length: 13 })
+		.$defaultFn(() => nanoid())
+		.primaryKey(),
+	name: varchar("name", { length: 32 }).unique().notNull(),
+	createdAt: timestamp("createdAt")
+		.default(sql`CURRENT_TIMESTAMP`)
+		.notNull(),
+	updatedAt: timestamp("updatedAt")
+		.default(sql`CURRENT_TIMESTAMP`)
+		.onUpdateNow()
+		.notNull(),
+	deletedAt: timestamp("deletedAt"),
+});
+
+export type Organisation = InferSelectModel<typeof organisations>;
+export interface Org extends InferSelectModel<typeof organisations> {}
+
+type Org1 = Pick<Organisation, "id" | "name">;
+export type { Org1 };
+type Org2 = Pick<Organisation, "id" | "name">;
+export { type Org2 };
+
+export async function getOrganisations(): Promise<
+	Pick<Organisation, "id" | "name">[]
+> {
+	return [
+		{
+			id: "1",
+			name: "Alice",
+		},
+		{
+			id: "2",
+			name: "Bob",
+		},
+	];
+	return db.select().from(organisations);
+}
