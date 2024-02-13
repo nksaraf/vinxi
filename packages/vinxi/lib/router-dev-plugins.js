df335da178337a3f28e9169cdd8b4ad0fe7dd02a
nksaraf <nsaraf98@gmail.com>
2024-02-14 01:48:36 +0530

    fix: cache dir should be in node_modules for vite to work properly, fixes #192

--- packages/vinxi/lib/router-dev-plugins.js
+++ packages/vinxi/lib/router-dev-plugins.js
@@ -20,7 +20,7 @@ export const ROUTER_MODE_DEV_PLUGINS = {
 			ssr: {
 				noExternal: ["vinxi"],
 			},
-			cacheDir: `.vinxi/cache/${router.name}`,
+			cacheDir: `node_modules/.vinxi/cache/${router.name}`,
 			optimizeDeps: {
 				exclude: ["vinxi"],
 			},
@@ -64,7 +64,7 @@ export const ROUTER_MODE_DEV_PLUGINS = {
 			ssr: {
 				noExternal: ["vinxi"],
 			},
-			cacheDir: `.vinxi/cache/${router.name}`,
+			cacheDir: `node_modules/.vinxi/cache/${router.name}`,
 			define: {
 				"process.env.TARGET": JSON.stringify(process.env.TARGET ?? "node"),
 			},
@@ -101,7 +101,7 @@ export const ROUTER_MODE_DEV_PLUGINS = {
 		manifest(),
 		config("appType", {
 			appType: "custom",
-			cacheDir: `.vinxi/cache/${router.name}`,
+			cacheDir: `node_modules/.vinxi/cache/${router.name}`,
 			ssr: {
 				noExternal: ["vinxi"],
 			},
