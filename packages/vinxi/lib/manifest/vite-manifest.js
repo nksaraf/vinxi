03f4cd8fbe4e637a4ab6c89e5e2ad7a33792dee4
Ryan Carniato <ryansolid@gmail.com>
2024-07-19 11:51:59 -0700

    fix #332 - dramatic performance improvement of findAssetsInViteManifest

--- packages/vinxi/lib/manifest/vite-manifest.js
+++ packages/vinxi/lib/manifest/vite-manifest.js
@@ -6,45 +6,39 @@
  * @param {any} manifest Client manifest
  * @param {string} id Chunk id
  * @param {Map<string, string[]>} assetMap Cache of assets
+ * @param {string[]} stack Stack of chunk ids to prevent circular dependencies
  * @returns Array of asset URLs
  */
-const findAssetsInViteManifest = (manifest, id, assetMap = new Map()) => {
-	const stack = [];
-
-	/**
-	 * @param {string} id
-	 */
-	function traverse(id) {
-		if (stack.includes(id)) {
-			return [];
-		}
-
-		const cached = assetMap.get(id);
-		if (cached) {
-			return cached;
-		}
-		const chunk = manifest[id];
-		if (!chunk) {
-			return [];
-		}
-
-		stack.push(id);
-
-		const assets = [
-			...(chunk.assets || []),
-			...(chunk.css || []),
-			...(chunk.imports?.flatMap(traverse) || []),
-		];
-		const imports = chunk.imports?.flatMap(traverse) || [];
-		const all = [...assets, ...imports].filter(Boolean);
-		all.push(chunk.file);
-		assetMap.set(id, all);
-
-		stack.pop();
-
-		return Array.from(new Set(all));
+function findAssetsInViteManifest(manifest, id, assetMap = new Map(), stack = []) {
+	if (stack.includes(id)) {
+		return [];
 	}
-	return traverse(id);
-};
+
+	const cached = assetMap.get(id);
+	if (cached) {
+		return cached;
+	}
+	const chunk = manifest[id];
+	if (!chunk) {
+		return [];
+	}
+
+	const assets = [
+		...(chunk.assets?.filter(Boolean) || []),
+		...(chunk.css?.filter(Boolean) || [])
+	];
+	if (chunk.imports) {
+		stack.push(id);
+		for (let i = 0, l = chunk.imports.length; i < l; i++) {
+			assets.push(...findAssetsInViteManifest(manifest, chunk.imports[i], assetMap, stack));
+		}
+		stack.pop();
+	}
+	assets.push(chunk.file);
+	const all = Array.from(new Set(assets));
+	assetMap.set(id, all);
+
+	return all;
+}
 
 export default findAssetsInViteManifest;
